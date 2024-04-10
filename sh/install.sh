6e8c9b41620c3286c25c292219f98ecdb42876d0
Grafies <54047540+grafies@users.noreply.github.com>
2024-04-10 15:33:39 +0800

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -2,7 +2,9 @@
 
 # 安装时间服务器
 yum -y install ntp
-ntpdate ntp.aliyun.com
+systemctl start ntpd
+systemctl enable ntpd
 
 # 添加到计划任务
-echo "* */1 * * * ntpdate ntp.aliyun.com" | crontab -
+# echo "* */1 * * * ntpdate ntp.aliyun.com" | crontab -
+
